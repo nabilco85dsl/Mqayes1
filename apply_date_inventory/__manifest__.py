dd9ff79474d59c8efdadf58179ff95cde2ad45fc
nabilco85dsl <121552971+nabilco85dsl@users.noreply.github.com>
2023-08-28 22:17:16 +0300

    apply_date_inventory
    add adj date of inventory report

---
+++ apply_date_inventory/__manifest__.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Apply date inventory",
+
+    'summary': """
+        This module is for Apply date inventory""",
+
+    'description': """
+       This module is for Apply date inventory"
+    """,
+
+    'author': "Said Kraim",
+    'category': 'Inventory/Inventory',
+    'version': '15.0.1',
+
+    'depends': ['stock', 'stock_account'],
+
+    'data': [
+        'views/stock_quant_views.xml',
+
+    ],
+    'license': 'LGPL-3',
+
+}
---
+++ apply_date_inventory/models/stock_quant.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, _
+from odoo.exceptions import UserError
+from odoo.tools.float_utils import float_compare
+
+
+class StockQuant(models.Model):
+    _inherit = "stock.quant"
+
+    def _apply_inventory(self):
+        move_vals = []
+        if not self.user_has_groups('stock.group_stock_manager'):
+            raise UserError(_('Only a stock manager can validate an inventory adjustment.'))
+        for quant in self:
+            # Create and validate a move so that the quant matches its `inventory_quantity`.
+            if float_compare(quant.inventory_diff_quantity, 0, precision_rounding=quant.product_uom_id.rounding) > 0:
+                move_vals.append(
+                    quant._get_inventory_move_values(quant.inventory_diff_quantity,
+                                                     quant.product_id.with_company(
+                                                         quant.company_id).property_stock_inventory,
+                                                     quant.location_id))
+            else:
+                move_vals.append(
+                    quant._get_inventory_move_values(-quant.inventory_diff_quantity,
+                                                     quant.location_id,
+                                                     quant.product_id.with_company(
+                                                         quant.company_id).property_stock_inventory,
+                                                     out=True))
+        moves = self.env['stock.move'].with_context(inventory_mode=False).create(move_vals)
+        moves._action_done()
+        self.location_id.write({'last_inventory_date': self.inventory_date})
+        date_by_location = {loc: loc._get_next_inventory_date() for loc in self.mapped('location_id')}
+        for quant in self:
+            quant.inventory_date = date_by_location[quant.location_id]
+        self.write({'inventory_quantity': 0, 'user_id': False})
+        self.write({'inventory_diff_quantity': 0})
